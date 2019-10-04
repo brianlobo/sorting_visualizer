73614d11ee1c5b38435897cb5401aa8da47824d9
Brian Lobo <brianlobo.code@gmail.com>
2019-10-04 19:50:47 -0400

    Added color to visualization, and ability for user to select the data size. Also organized files

---
+++ js/sketch.js
@@ -0,0 +1,105 @@
+let i = 0;
+let rectWidth;
+let states = [];
+let go = document.getElementById('go');
+
+function setup() {
+    let numOfRects = document.getElementById('numOfRects').value;
+    let width = document.getElementById('canvas').offsetWidth;
+    let height = document.getElementById('canvas').offsetHeight;
+    let canvas = createCanvas(width, height);
+
+    rectWidth = floor(width / numOfRects);
+
+    canvas.parent('canvas');
+    values = new Array(floor(width / rectWidth));
+
+    for (let i = 0; i < values.length; i++) {
+        values[i] = random(height);
+        states[i] = -1;
+    }
+
+    frameRate(100);
+    quickSort(values, 0, values.length - 1);
+}
+
+function draw() {
+    background(23);
+
+    for (let i = 0; i < values.length; i++) {
+        stroke(0);
+        if (states[i] == 0) {
+            fill('#E0777D');
+        } else if (states[i] == 1) {
+            fill('#D6FFB7');
+        } else {
+            fill(255);
+        }
+        rect(i * rectWidth, height - values[i], rectWidth, values[i]);
+    }
+}
+
+// QuickSort func
+async function quickSort(arr, start, end) {
+    if (start >= end) {
+        return;
+    }
+
+    let index = await partition(arr, start, end);
+    states[index] = -1;
+    await Promise.all([
+        quickSort(arr, start, index - 1),
+        quickSort(arr, index + 1, end)
+    ]);
+}
+// Helper function for quicksort
+async function partition(arr, start, end) {
+    for (let i = start; i < end; i++) {
+        states[i] = 1;
+    }
+
+    let pivotIndex = start;
+    let pivotVal = arr[end];
+
+    for (let i = start; i < end; i++) {
+        if (arr[i] < pivotVal) {
+            await swapVal(arr, i, pivotIndex);
+            states[pivotIndex] = -1;
+            pivotIndex++;
+            states[pivotIndex] = 0;
+        }
+    }
+    await swapVal(arr, pivotIndex, end);
+
+    for (let i = start; i < end; i++) {
+        if (i != pivotIndex) {
+            states[i] = -1;
+        }
+    }
+
+    return pivotIndex;
+}
+
+// Swaps two vals in a given array
+async function swapVal(arr, a, b) {
+    await sleep(20);
+
+    let temp = arr[a];
+    arr[a] = arr[b];
+    arr[b] = temp;
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+// Event Listeners //
+window.addEventListener("resize", function () {
+    setup();
+    draw();
+});
+
+go.addEventListener("click", function () {
+    setup();
+    draw();
+});
